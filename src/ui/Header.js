a4a90a8cb2af983892006b20689420f3fb4c57c3
sibusiso matsini <sbu.matsini@gmail.com>
2020-08-01 05:28:00 +0200

    S2#19 appBar Logo | remove default toolbar padding[disableGutters], increase theme toolbar marginBottom for taller toolbar

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { useScrollTrigger, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import logo from '../assets/logo.svg';
+
 const useStyles = makeStyles(theme => ({
     toolbarMargin: {
-        ...theme.mixins.toolbar
+        ...theme.mixins.toolbar,
+        marginBottom: '3em'
     },
+    logo: {
+        height: '7em'
+    }
 }));
 
 function ElevationScroll(props) {
@@ -31,10 +36,8 @@ export default function Header(props) {
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position='fixed' >
-                    <Toolbar>
-                        <Typography variant='h4'>
-                            Arc Development
-                    </Typography>
+                    <Toolbar disableGutters>
+                        <img className={classes.logo} alt="company logo" src={logo}/>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
