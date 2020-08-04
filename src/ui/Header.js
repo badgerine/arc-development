84c395a5349d719fb085035b953bd6acacef51e8
sibusiso matsini <sbu.matsini@gmail.com>
2020-08-04 05:38:43 +0200

    S2#27 active tab refresh fix

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { useScrollTrigger } from '@material-ui/core';
@@ -15,7 +15,13 @@ const useStyles = makeStyles(theme => ({
         marginBottom: '3em'
     },
     logo: {
-        height: '7em'
+        height: '8em'
+    },
+    logoContainer: {
+        padding: 0,
+        '&:hover': {
+            backgroundColor: 'transparent'
+        },
     },
     tabContainer: {
         marginLeft: 'auto'
@@ -50,6 +56,22 @@ export default function Header(props) {
     const classes = useStyles();
     const [value, setValue] = useState(0);
 
+    useEffect(() => {
+        if (window.location.pathname === '/' && value !== 0) {
+            setValue(0);
+        } else if (window.location.pathname === '/services' && value !== 1) {
+            setValue(1);
+        } else if (window.location.pathname === '/revolution' && value !== 2) {
+            setValue(2);
+        } else if (window.location.pathname === '/about' && value !== 3) {
+            setValue(3);
+        } else if (window.location.pathname === '/contact' && value !== 4) {
+            setValue(4);
+        } else if (window.location.pathname === '/estimate' && value !== 5) {
+            setValue(5);
+        }
+    }, [value])
+
     const handleChange = (e, value) => {
         setValue(value);
     }
@@ -59,7 +81,10 @@ export default function Header(props) {
             <ElevationScroll>
                 <AppBar position='fixed' >
                     <Toolbar disableGutters>
-                        <img className={classes.logo} alt="company logo" src={logo} />
+                        <Button disableRipple component={Link} to='/' className={classes.logoContainer} onClick={() => setValue(0)}>
+                            <img className={classes.logo} alt="company logo" src={logo} />
+                        </Button>
+
                         <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor='primary'>
                             <Tab className={classes.tab} component={Link} to='/' label="Home" />
                             <Tab className={classes.tab} component={Link} to='/services' label="Services" />
