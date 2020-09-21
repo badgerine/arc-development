99a8b67e882f118cad429c2a92b7c5c950752cff
sibusiso matsini <sbu.matsini@gmail.com>
2020-09-21 10:28:37 +0200

    S4#53 using grids - setup

--- src/ui/Footer.js
+++ src/ui/Footer.js
@@ -1,28 +1,77 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 import footerAdornment from '../assets/Footer_Adornment.svg';
 
 const useStyles = makeStyles(theme => ({
-    footer: {
-        backgroundColor: theme.palette.common.arcBlue,
-        width: '100%',
-        zIndex: '1302',
-        position: 'relative'
+  footer: {
+    backgroundColor: theme.palette.common.arcBlue,
+    width: '100%',
+    zIndex: '1302',
+    position: 'relative'
+  },
+  adornment: {
+    width: '25em',
+    verticalAlign: 'bottom',
+    [theme.breakpoints.down('md')]: {
+      width: '21em'
     },
-    adornment: {
-        width: '25em',
-        verticalAlign: 'bottom',
-        [theme.breakpoints.down('md')]: {
-            width: '21em'
-        },
-        [theme.breakpoints.down('xs')]: {
-            width: '15em'
-        }
+    [theme.breakpoints.down('xs')]: {
+      width: '15em'
     }
+  },
+  mainContainer: {
+    position: 'absolute',
+  },
+  link: {
+    color: 'white',
+    fontFamily: 'Arial',
+    fontSize: '0.75rem',
+    fontWeight: 'bold',
+  }
 }));
 
 export default (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return <footer className={classes.footer}><img className={classes.adornment} alt='black decorative slash' src={footerAdornment} /></footer>
+  return (
+    <footer className={classes.footer}>
+      <Grid container justify='center' className={classes.mainContainer}>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>Home</Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>Services</Grid>
+            <Grid item className={classes.link}>Custom Software Development</Grid>
+            <Grid item className={classes.link}>Mobile App Development</Grid>
+            <Grid item className={classes.link}>Website Development</Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>The Revolution</Grid>
+            <Grid item className={classes.link}>Vision</Grid>
+            <Grid item className={classes.link}>Technology</Grid>
+            <Grid item className={classes.link}>Process</Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>About Us</Grid>
+            <Grid item className={classes.link}>History</Grid>
+            <Grid item className={classes.link}>Team</Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction='column'>
+            <Grid item className={classes.link}>Contact Us</Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <img className={classes.adornment} alt='black decorative slash' src={footerAdornment} />
+    </footer>)
 }
