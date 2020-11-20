244eae4527469bb09fd79aa52c9b532592b5d999
sibusiso matsini <sbu.matsini@gmail.com>
2020-11-20 15:57:32 +0200

    S8#124 using node mailer

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+const config = functions.config();
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport(
+  {
+    service: 'Gmail',
+    auth: {
+      user: config.user.email,
+      pass: config.user.password
+    },
+  });
+
+let mailOptions = {
+  from: 'MaterialUI Course',
+  to: 'sbu.matsini@gmail.com',
+  subject: 'testing node mailer',
+  text: 'It is what it is.'
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendMail = functions.https.onRequest((request, response) => {
+  transporter.sendMail(mailOptions, error => {
+    if (error) {
+      response.send(error)
+    } else {
+      response.send('Message sent successfully.')
+    }
+  })
+});
